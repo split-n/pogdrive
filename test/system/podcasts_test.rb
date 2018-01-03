2575b97251fdc8b8d412f5ad49845a0d99b091ad
s <git@tnu.jp>
2018-01-04 01:58:57 +0900

    scaffold podcast

---
+++ test/system/podcasts_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class PodcastsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit podcasts_url
+  #
+  #   assert_selector "h1", text: "Podcast"
+  # end
+end
